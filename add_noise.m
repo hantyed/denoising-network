505c64e1a2a80a179f9feb85e4f52b84878c09b6
Hannah Tyedmers <48016552+hantyed@users.noreply.github.com>
2022-10-14 17:30:19 +0200

    NMF updated

---
+++ add_noise.m
@@ -0,0 +1,21 @@
+function noisyAudio = addNoise(audio, noise)
+
+% Mix noise into clean audio signal 
+
+if length(audio) > length(noise)
+    vertcat(noise, noise);
+end
+
+%get random initial value from noise signal
+randind = randi(numel(noise) - numel(audio), [1 1]);
+
+%get segment from noise corresponding to length of clean audio signal
+noiseSegment = noise(randind:randind + numel(audio) - 1);
+
+speechPower = sum(audio.^2);
+noisePower = sum(noiseSegment.^2);
+
+noiseSegment = noiseSegment.*sqrt(speechPower/noisePower);
+
+noisyAudio = audio + noiseSegment;
+
--- nmf_speech.m
+++ nmf_speech.m
@@ -1,32 +1,72 @@
 %NMF dictionary of speech signals
 
 clear, %close all;
+doSampleRateConvert = false; %whether or not the audio is downsampled
+unknownInput = false; %whether 
 
 %import data to matrices
 %[input, fs] = audioread('speech_example.wav');
-%[trainNoise, fsNoise] = audioread('mainsBrum50Hz.wav');
-%[trainNoise, fsNoise] = audioread("WashingMachine-16-8-mono-1000secs.mp3");
-trainSpeech = getSpeechExamples;
+%[trainNoise, fsNoise] = audioread('./nmfData/mainsBrum50Hz.wav');
+[trainNoise, fsNoise] = audioread("WashingMachine-16-8-mono-1000secs.mp3");
 
-%trainData = vertcat(trainSpeech, trainNoise);
+samplesIncluded = 5;
+trainSpeech = getSpeechSamples(samplesIncluded);
 
-%Train = cell2mat(trainSpeech.Files);
-
-fs = 48000;
-
+%get all samples from data storage and convert to a vector
 T = readall(trainSpeech);
+audioData = cell2mat(T);
 
-trainData = cell2mat(T);
+%convert the sample rate of the speech signals if needed
+if doSampleRateConvert
 
-%trainData = T;
-input = trainData;
+    inputFs = 48000;
+    fs = 8000;
+    bandwidth = 7920; %CHECK: why this value?
+    
+    sampleRateConv = dsp.SampleRateConverter('InputSampleRate', inputFs,...
+        'OutputSampleRate', fs, 'Bandwidth', bandwidth);
 
-% aud = zeros(size(T,1));
-% fs = zeros(size(T,1));
-% 
-% for ii=1:size(T,1)
-%     [aud(ii), fs(ii)] = read(trainSpeech);
-% end
+    audioData = sampleRateConv(audioData);
+
+else
+
+    fs = 48000;
+
+end
+
+%convert the sample rate of the noise signal to be the same as audioData
+if fsNoise ~= fs
+ 
+    inputFs = fsNoise;
+    bandwidth = 7920;
+    decimationFactor = inputFs/fs;
+    L = floor(numel(trainNoise)/decimationFactor);
+    trainNoise = trainNoise(1:decimationFactor*L);
+
+    sampleRateConv = dsp.SampleRateConverter('InputSampleRate', inputFs,...
+        'OutputSampleRate', fs, 'Bandwidth', bandwidth);
+
+    trainNoise = sampleRateConv(trainNoise);
+end
+
+%%
+
+%add noise to the signals
+
+if unknownInput
+    inputInit = getSpeechSamples(2);
+    inputInit = readall(inputInit);
+    inputInit = cell2mat(inputInit);
+
+    input = add_noise(inputInit, trainNoise);
+else
+
+    input = add_noise(audioData, trainNoise);
+end
+
+trainData = vertcat(audioData,trainNoise(1:fs*2));
+
+%% set data parameters
 
 %specify number of sources
 sources = 5;
@@ -34,75 +74,89 @@ sources = 5;
 %windowing conditions
 segmentTime = 0.030;
 segmentLength = round(segmentTime*fs);
-overlap = round(segmentLength*0.5);
-dftSize = round(4069*0.5);
+overlap = round(segmentLength*0.75);
+dftSize = round(4069*2);
 
 window = sqrt(hann(segmentLength, 'periodic'));
 
 %% compute STFT/magnitude of training data
 
-stftTrain = stft(trainData, fs, ...
+stftTrain = stft(audioData, fs, ...
     'Window', window, 'OverlapLength', overlap, ...
     'FFTLength', dftSize, 'FrequencyRange', 'onesided');
 
 powerSpecT = abs(stftTrain).^2/length(window);
 
 %% NMF
+%TODO: figure out WHY output sometimes turns out NAN
 
 %initial dictionary A and activations X
 A_train = rand(size(powerSpecT,1),sources);
 X_train = rand(sources,size(powerSpecT,2));
 
 iterations = 200;
-beta = 1;
+beta = 2;
 
-% Unsupervised training to obtain dictionary (atoms of piano notes)
+% Unsupervised training to obtain dictionary
 
 [A_train, X_train, cost_T] = unsupervisedNmfTrain(powerSpecT, ...
     A_train, X_train, iterations, beta);
 
+
+%%
+%plotX(X_train, sources)
+%plotX(activationsSummed, sources)
+
 %% Supervised training to obtain activation matrix from recording (using known dictionary)
+%this whole block seems to be redundant when training and input data are the same 
 
 %compute STFT/magnitudes of input signal/recording
-
-stftRec = stft(input, fs, ...
+stftInput = stft(input, fs, ...
     'Window', window, 'OverlapLength', overlap, ...
     'FFTLength', dftSize, 'FrequencyRange', 'onesided');
 
-powerSpecRec = abs(stftRec).^2/length(window);
+powerSpecIn = abs(stftInput).^2/length(window);
 
-X_rec = rand(sources,size(powerSpecRec,2));
+X_input = rand(sources,size(powerSpecIn,2));
 
-[X_rec, cost_R] = supervisedNmfTrain(powerSpecRec, A_train, X_rec, iterations, beta);
-    
+[X_input, cost_R] = supervisedNmfTrain(powerSpecIn, A_train, X_input, iterations, beta);
+
 %% seperate sources with Wiener filtering 
 
-filteredSources = sourceSeperation(stftRec, A_train, X_rec, window, overlap);
-    
-%% Plot spectrograms
+filteredSources = sourceSeperation(stftInput, A_train, X_input, window, overlap);
 
-% f1 = figure;
-% spectrogram(filteredSources(1,:),window,overlap,dftSize,'yaxis');
-% 
-% f2 = figure;
-% spectrogram(filteredSources(2,:),window,overlap,dftSize,'yaxis');
+%% plot spectrogram of each source
+
+plotSpec(filteredSources,sources,window,overlap,dftSize)
+
+%% combine wanted sources
+
+x = 1;
+nosySources=[1 2 3]; %I estimate this from looking at the spectrograms each time...
+
+for ii=1:sources
+    if ii ~= nosySources
+        x = x + filteredSources(ii,:);
+    end
+end
+%sound(x,fs)
 
 %% NMF for identifying dictionary and activations (unsupervised training)
 
-function [A, X, cost] = unsupervisedNmfTrain(S, A, X, iterations, beta)
+function [A, X, cost] = unsupervisedNmfTrain(powerSpectrum, A, X, iterations, beta)
 
     ap = A*X; %approximation of signal inputs S
-    [F,N] = size(S);
+    [F,N] = size(powerSpectrum);
     
     cost = zeros(1,iterations);
-    cost(1) = betaDivergenceCost(S, ap, beta);
+    cost(1) = betaDivergenceCost(powerSpectrum, ap, beta);
 
     for i=2:iterations
 
-        A = A .* ((S .* ap.^(beta-2))* X') ./ (ap.^(beta-1) * X');
+        A = A .* ((powerSpectrum .* ap.^(beta-2))* X') ./ (ap.^(beta-1) * X');
         ap = A*X; %recalculate approximate
 
-        X = X .* (A'*(ap .^ (beta-2) .* S)) ./ (A'*ap.^ (beta-1));
+        X = X .* (A'*(ap .^ (beta-2) .* powerSpectrum)) ./ (A'*ap.^ (beta-1));
         ap = A*X;
 
         %normalisation of matrices
@@ -111,49 +165,51 @@ function [A, X, cost] = unsupervisedNmfTrain(S, A, X, iterations, beta)
         X = X .* repmat(scale',1,N);
 
         % Compute cost value
-        cost(iterations) = betaDivergenceCost(S, ap ,beta);
+        cost(iterations) = betaDivergenceCost(powerSpectrum, ap ,beta);
 
     end   
 end
 
 %% Find activations 
 
-function [Act, cost] = supervisedNmfTrain(S, dict, Act, iterations, beta)
+function [X, cost] = supervisedNmfTrain(powerSpectrum, A, X, iterations, beta)
 
-    ap = dict*Act; %approximation of input signal
+    ap = A*X; %approximation of input signal
     
     cost = zeros(1,iterations);
-    cost(1) = betaDivergenceCost(S, ap, beta);
+    cost(1) = betaDivergenceCost(powerSpectrum, ap, beta);
 
     for i=2:iterations
         
         %update activation
-        Act = Act .* (dict'*(ap .^ (beta-2) .* S)) ./ (dict'*ap.^ (beta-1));
-        ap = dict*Act;
+        X = X .* (A'*(ap .^ (beta-2) .* powerSpectrum)) ./ (A'*ap.^ (beta-1));
+        ap = A*X;
         
         %compute cost value
-        cost(iterations) = betaDivergenceCost(S, ap, beta);
+        cost(iterations) = betaDivergenceCost(powerSpectrum, ap, beta);
         
     end
 end
 
 %% Find cost of divergence depending on value of beta
 
-function cost = betaDivergenceCost(S, S_ap, beta)
+function cost = betaDivergenceCost(powerSpectrum, S_ap, beta)
 
     if beta == 0 %IS
-        cost = sum(S(:) ./ S_ap(:) - log10( S(:) ./ S_ap(:) ) - 1 );
+        cost = sum(powerSpectrum(:) ./ S_ap(:) - log10( powerSpectrum(:) ...
+            ./ S_ap(:) ) - 1 );
         
     elseif beta == 1 % KL
-        cost = sum(S(:) .* log( S(:) ./ S_ap(:) ) - S(:) + S_ap(:));
+        cost = sum(powerSpectrum(:) .* log( powerSpectrum(:) ...
+            ./ S_ap(:) ) - powerSpectrum(:) + S_ap(:));
 
     elseif beta == 2 %Euclidean (squared error)
-        cost = sum( 0.5 * (S(:) - S_ap(:)).^2 );
+        cost = sum( 0.5 * (powerSpectrum(:) - S_ap(:)).^2 );
         
     end
 end
 
-%%
+%% Seperation with wiener filter
 
 function sourceMatrix = sourceSeperation(stft, A, X, window, overlap)
 
@@ -170,7 +226,7 @@ function sourceMatrix = sourceSeperation(stft, A, X, window, overlap)
     for i = 1:nSources
         
         %Wiener reconstruction
-        halfStft = stft .* (A(:,i) * X(i,:)) ./ ap;
+        halfStft = stft .* (A(:,i) * X(i,:)) ./ dynamicRangeLimiting(ap,120);
         
         %create dublicate freq components
         fullStft = [halfStft; flipud(conj(halfStft(2:end-1,:)))];
@@ -183,7 +239,6 @@ function sourceMatrix = sourceSeperation(stft, A, X, window, overlap)
         
         for s = 1:nSegments
             
-            
             sourceMatrix(i,index) = sourceMatrix(i,index) + ...
                 (window.*separatedSourceMatrix(:,s))';
             
@@ -194,17 +249,83 @@ function sourceMatrix = sourceSeperation(stft, A, X, window, overlap)
     
 end
 
-%%
+%% dynamicRangeLimiting from SMSA to diminish the amplitude (to be modified?)
 
-function trainDataVoices = getSpeechExamples
+function limitedNonnegativeData = ...
+    dynamicRangeLimiting(nonnegativeData, maxRangeDb)
 
-dataset = fullfile("voiceData","commonvoice"); 
-trainDataVoices = audioDatastore(fullfile(dataset,"train"),"IncludeSubfolders",true);
+    logPowerSpectrum = 10*log10(nonnegativeData);
 
-%[noise, fsNoise] = audioread("WashingMachine-16-8-mono-1000secs.mp3");
+    limitedLogPowerSpectrum = max(logPowerSpectrum, ...
+            max(max(logPowerSpectrum) - maxRangeDb));
 
-samplesIncluded = 5;
-trainDataVoices = shuffle(trainDataVoices);
-trainDataVoices = subset(trainDataVoices,1:samplesIncluded); 
+    limitedNonnegativeData = 10.^(limitedLogPowerSpectrum/10);
+end
 
+%% load data
+
+function trainDataVoices = getSpeechSamples(samplesIncluded)
+
+    dataset = fullfile("voiceData","commonvoice"); 
+    trainDataVoices = audioDatastore(fullfile(dataset,"train"),"IncludeSubfolders",true);
+    
+    %[noise, fsNoise] = audioread("WashingMachine-16-8-mono-1000secs.mp3");
+    
+    trainDataVoices = shuffle(trainDataVoices);
+    trainDataVoices = subset(trainDataVoices,1:samplesIncluded); 
+
+end
+
+%% plottig helpers
+function plotX(X,times)
+
+    figure,
+    
+    for ii=1:times
+    
+        hold on, plot(X(ii,:))
+    end
+end
+
+function plotSpec(X,times,window,overlap,dftSize)
+
+    for ii =1:times
+
+        figure, spectrogram(X(ii,:),window,overlap,dftSize,'yaxis');
+    end
+end
+
+%% "Cheat" - Remove the activations that are not of interest 
+%(if we know when different sources are active in which sections of the
+%training data, i.e. 
+
+
+function [activations, activationsSummed] = pickActivationsBySections(X, T, samplesIncluded)
+% X = activation matrix, T = data cell
+
+    activations = zeros(size(X));
+    activationsSummed = zeros(size(X));
+    prevFrames = 1;
+    
+    for n = 1:samplesIncluded
+        %rounding errors might occur for these values since they need to be
+        %integers... condition check beforehand might help
+        sampleSize = round(size(T{n},1));
+        procentage = round(sampleSize / size(input,1) * 100);
+    
+        sampleTimeFrame = round((size(X,2) * procentage)/100);
+    
+        %get activations for each dictionary entry (but here we assume they match one)
+        activations(n, prevFrames:prevFrames+sampleTimeFrame) = ...
+            X(n, prevFrames:prevFrames+sampleTimeFrame);
+    
+        for s = 1:samplesIncluded %combine all activations across all dictionary entries
+            activationsSummed(s, prevFrames:prevFrames+sampleTimeFrame) = ...
+                activationsSummed(s, prevFrames:prevFrames+sampleTimeFrame) + ...
+                X(s, prevFrames:prevFrames+sampleTimeFrame);
+        end
+    
+        prevFrames = prevFrames + sampleTimeFrame;
+    
+    end
 end
